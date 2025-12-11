58893285fe81394ce0ae1ea1dff8563bde473083
SecretGillian <secret.gillian.S3la@gmail.com>
2025-12-11 08:11:25 +0100

    ajout de la mise à jour de l'heur de la raspberry

--- src/main.c
+++ src/main.c
@@ -14,8 +14,7 @@ int main(void)
     InitRTC();
     
     l_TimeVal_Test = GetTimeRTC();
-    printf("on est le %d/%d/2025 et il est %d:%d:%d\n", l_TimeVal_Test.m_RtcValue_mDay.m_uint8_val, l_TimeVal_Test.m_RtcValue_mon.m_uint8_val, l_TimeVal_Test.m_RtcValue_hour.m_uint8_val, l_TimeVal_Test.m_RtcValue_min.m_uint8_val, l_TimeVal_Test.m_RtcValue_sec.m_uint8_val);
-
+    
     l_pTm_SetTemps.tm_sec   = l_TimeVal_Test.m_RtcValue_sec.m_uint8_val;
     l_pTm_SetTemps.tm_min   = l_TimeVal_Test.m_RtcValue_min.m_uint8_val;
     l_pTm_SetTemps.tm_hour  = l_TimeVal_Test.m_RtcValue_hour.m_uint8_val;
@@ -26,14 +25,12 @@ int main(void)
     l_pTm_SetTemps.tm_yday  = 0;
     l_pTm_SetTemps.tm_year  = 2025 - 1900;
 
-    printf("mktime \n");
     l_time_TimeVal = mktime(&l_pTm_SetTemps);
       
     /* Now set the clock to this time */
     l_timeval_SetTime.tv_sec = l_time_TimeVal;
     l_timeval_SetTime.tv_usec = 0;
 
-    printf("settimeofday taille int : %d \n", sizeof(int));
     l_int_return = settimeofday(&l_timeval_SetTime,NULL);
 
     /* Check that the change was successful */
