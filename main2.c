8512419f90c81d8a867ded940584a5caea3f40b8
SecretGillian <secret.gillian.S3la@gmail.com>
2025-12-19 11:25:11 +0100

    modification du service InitSysClock

--- main2.c
+++ main2.c
@@ -1,79 +1,48 @@
-#include <systemd/sd-bus.h>
-#include <stdio.h>
-#include <unistd.h>
+/* #include <stdio.h>
+#include <sys/stat.h>
+#include <time.h>
 
 int main() {
-    sd_bus *bus = NULL;
-    sd_bus_error error = SD_BUS_ERROR_NULL;
-    int synced = 0;
-    int r;
+    struct stat st;
+    const char *file = "/run/systemd/timesync/synchronized";
 
-    /* Connexion au bus système */
-    r = sd_bus_default_system(&bus);
-    if (r < 0) {
-        fprintf(stderr, "Erreur bus system: %s\n", strerror(-r));
-        return -1;
-    }
-
-    /* Lecture de la propriété NTPSynchronized */
-    r = sd_bus_get_property_trivial(
-        bus,
-        "org.freedesktop.timedate1",        /* service */
-        "/org/freedesktop/timedate1",       /* objet */
-        "org.freedesktop.timedate1",        /* interface */
-        "NTPSynchronized",                  /* propriété */
-        &error,
-        'b',                                /* signature D-Bus : boolean */
-        &synced                             /* stocké dans int */
-    );
-
-    if (r < 0) {
-        fprintf(stderr, "Erreur lecture NTPSynchronized: %s\n",
-                error.message ? error.message : strerror(-r));
-        sd_bus_error_free(&error);
-        sd_bus_unref(bus);
-        return -1;
-    }
-
-    sd_bus_unref(bus);
-
-    printf("valeur : %d\n", synced);
-
-    if (synced == 1) {
-        printf("NTP synchronisé\n");
-    } else if (synced == 0) {
-        printf("NTP NON synchronisé\n");
-    } else {
-        printf("Erreur D-Bus\n");
-    }
-
-    /* sd_bus *bus = NULL;
-    sd_bus_default_system(&bus);
-
-    int synced = 0;
-    int r = sd_bus_get_property_trivial(
-    bus,
-    "org.freedesktop.timedate1",      // bus name
-    "/org/freedesktop/timedate1",     // object path
-    "org.freedesktop.timedate1",      // interface
-    "NTPSynchronized",                 // property
-    NULL,                              // error (sd_bus_error*) si tu veux
-    'b',                               // signature D-Bus (bool)
-    &synced
-);
-
-
-    if (r < 0) {
-        fprintf(stderr, "Erreur lecture NTPSynchronized: %s\n", strerror(-r));
+    if (stat(file, &st) != 0) {
+        perror("stat");
         return 1;
     }
 
-    if (synced)
-        printf("NTP synchronisé\n");
-    else
-        printf("NTP perdu ou non synchronisé\n");
+    time_t now = time(NULL);
+    double delta = difftime(now, st.st_mtime);
 
+    printf("Dernière mise à jour : %.0f secondes\n", delta);
 
-    sd_bus_unref(bus);
-    return 0; */
+    if (delta < 300) {
+        printf("Synchronisation NTP récente\n");
+    } else {
+        printf("Synchronisation NTP ancienne ou perdue\n");
+    }
+
+    return 0;
+} */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <errno.h>
+
+int main() {
+    const char *path = "/home/user/test_i2c/test.txt"/* "/run/systemd/timesync/synchronized" */;
+
+    if (unlink(path) == 0) {
+        printf("Fichier supprimé avec succès\n");
+    } else {
+        if (errno == ENOENT) {
+            printf("Le fichier n'existe pas\n");
+        } else if (errno == EACCES || errno == EPERM) {
+            perror("Permission refusée");
+        } else {
+            perror("Erreur unlink");
+        }
+    }
+
+    return 0;
 }
--- src/main.c
+++ src/main.c
@@ -14,11 +14,6 @@ int main(void)
 
     int l_int_return                = 0;
 
-    const char l_str_host[]         = "8.8.8.8";
-    uint8_t l_uint8_port            = 53;
-    int l_int_sock                  = 0;
-    struct sockaddr_in l_StockaddrIn_server;
-    struct timeval l_timeval_timeout;
     uint8_t l_uint8_Test            = 0;
 
     FILE* l_PtrFILE_DebugFile       = NULL;
@@ -34,148 +29,108 @@ int main(void)
     } while (l_PtrFILE_DebugFile == NULL && l_uint8_Test <= 200);
 
     if(l_PtrFILE_DebugFile == NULL)
-    {
-        fprintf(l_PtrFILE_DebugFile, "écheque de l'ouverture du fichier de debug\n");
-        
+    {        
         return 1;
     }
     /**/
-
-    /**/ //création d'un socket réseau
-    do
-    {
-        l_int_sock = socket(AF_INET, SOCK_STREAM, 0);
-
-        l_uint8_Test++;
-    }while (l_int_sock < 0 && l_uint8_Test <= 200);
-
-    l_uint8_Test = 0;
     
-    if (l_int_sock < 0) {
-        fprintf(l_PtrFILE_DebugFile, "écheque de la création du socket réseau");
-        fclose(l_PtrFILE_DebugFile);
-        return 2;
-    }
-    /**/
-
-    /**/ //initialisation du time out pour connect
-    l_timeval_timeout.tv_sec = TIME_OUT_CONNECT;
-    l_timeval_timeout.tv_usec = 0;
-
-    setsockopt(l_int_sock, SOL_SOCKET, SO_SNDTIMEO, &l_timeval_timeout, sizeof(l_timeval_timeout));
-    /**/
-
-    /**/ //initialisation de la strucutre peretant d'effectuer le pig
-    l_StockaddrIn_server.sin_family = AF_INET;
-    l_StockaddrIn_server.sin_port = htons(l_uint8_port);
-    inet_pton(AF_INET, l_str_host, &l_StockaddrIn_server.sin_addr);
-    /**/
-
     // printf("Test de connexion vers %s:%d...\n", l_str_host, l_uint8_port);
 
     //test la connexion 
-    if (connect(l_int_sock, (struct sockaddr*)&l_StockaddrIn_server, sizeof(l_StockaddrIn_server)) == 0) {
-        fprintf(l_PtrFILE_DebugFile, "réseau fonctionnel.\n");
-        close(l_int_sock);
-        fclose(l_PtrFILE_DebugFile);
-        return 0;
-    } else {
-        fprintf(l_PtrFILE_DebugFile, "réseau non fonctionel. debut de l'initialisation par la RTC:\n");
+    fprintf(l_PtrFILE_DebugFile, "debut de l'initialisation par la RTC:\n");
 
-        //initialisation de la RTC
-        l_int_return = InitRTC();
+    //initialisation de la RTC
+    l_int_return = InitRTC();
 
-        switch (l_int_return)
+    switch (l_int_return)
+    {
+        case OPEN_FILE_ERROR:
+            fprintf(l_PtrFILE_DebugFile, "Erreur ouverture du bus I2C. fonction InitRTC\n");
+            fclose(l_PtrFILE_DebugFile);
+            return 2;
+        break;
+        
+        case SET_SLAVE_ERROR:
+            fprintf(l_PtrFILE_DebugFile, "Erreur sélection périphérique I2C. fonction InitRTC\n");
+            fclose(l_PtrFILE_DebugFile);
+            return 3;
+        break;
+
+        case SEND_VALUE_ERROR:
+            fprintf(l_PtrFILE_DebugFile, "Erreur écriture I2C. fonction InitRTC\n");
+            fclose(l_PtrFILE_DebugFile);
+            return 4;
+        break;
+
+        default:
+        break;
+    }
+    
+    /**/ //récupération des valeur de la RTC
+    l_TimeVal_ValRTC = GetTimeRTC();
+
+    if(l_TimeVal_ValRTC.m_aRtcValue_Tab[0] == 0)
+    {
+        switch (l_TimeVal_ValRTC.m_aRtcValue_Tab[1])
         {
             case OPEN_FILE_ERROR:
-                fprintf(l_PtrFILE_DebugFile, "Erreur ouverture du bus I2C. fonction InitRTC\n");
-                close(l_int_sock);
+                fprintf(l_PtrFILE_DebugFile, "Erreur ouverture du bus I2C. fonction GetTimeRTC\n");
                 fclose(l_PtrFILE_DebugFile);
+                return 2;
             break;
             
             case SET_SLAVE_ERROR:
-                fprintf(l_PtrFILE_DebugFile, "Erreur sélection périphérique I2C. fonction InitRTC\n");
-                close(l_int_sock);
+                fprintf(l_PtrFILE_DebugFile, "Erreur sélection périphérique I2C. fonction GetTimeRTC\n");
                 fclose(l_PtrFILE_DebugFile);
+                return 3;
             break;
 
             case SEND_VALUE_ERROR:
-                fprintf(l_PtrFILE_DebugFile, "Erreur écriture I2C. fonction InitRTC\n");
-                close(l_int_sock);
+                fprintf(l_PtrFILE_DebugFile, "Erreur écriture I2C. fonction GetTimeRTC\n");
                 fclose(l_PtrFILE_DebugFile);
+                return 4;
+            break;
+
+            case RECEIVE_VALUE_ERROR:
+                fprintf(l_PtrFILE_DebugFile, "Erreur lecture I2C. fonction GetTimeRTC\n");
+                fclose(l_PtrFILE_DebugFile);
+                return 5;
             break;
 
             default:
             break;
         }
-    
-        /**/ //récupération des valeur de la RTC
-        l_TimeVal_ValRTC = GetTimeRTC();
-
-        if(l_TimeVal_ValRTC.m_aRtcValue_Tab[0] == 0)
-        {
-            switch (l_TimeVal_ValRTC.m_aRtcValue_Tab[1])
-            {
-                case OPEN_FILE_ERROR:
-                    fprintf(l_PtrFILE_DebugFile, "Erreur ouverture du bus I2C. fonction GetTimeRTC\n");
-                    close(l_int_sock);
-                    fclose(l_PtrFILE_DebugFile);
-                break;
-                
-                case SET_SLAVE_ERROR:
-                    fprintf(l_PtrFILE_DebugFile, "Erreur sélection périphérique I2C. fonction GetTimeRTC\n");
-                    close(l_int_sock);
-                    fclose(l_PtrFILE_DebugFile);
-                break;
-
-                case SEND_VALUE_ERROR:
-                    fprintf(l_PtrFILE_DebugFile, "Erreur écriture I2C. fonction GetTimeRTC\n");
-                    close(l_int_sock);
-                    fclose(l_PtrFILE_DebugFile);
-                break;
-
-                case RECEIVE_VALUE_ERROR:
-                    fprintf(l_PtrFILE_DebugFile, "Erreur lecture I2C. fonction GetTimeRTC\n");
-                    close(l_int_sock);
-                    fclose(l_PtrFILE_DebugFile);
-                break;
-
-                default:
-                break;
-            }
-        }
-        /**/
-        
-        /**/ //initialise la structure de temps
-        l_pTm_SetTemps.tm_sec   = l_TimeVal_ValRTC.m_RtcValue_sec.m_uint8_val;
-        l_pTm_SetTemps.tm_min   = l_TimeVal_ValRTC.m_RtcValue_min.m_uint8_val;
-        l_pTm_SetTemps.tm_hour  = l_TimeVal_ValRTC.m_RtcValue_hour.m_uint8_val;
-        l_pTm_SetTemps.tm_mday  = l_TimeVal_ValRTC.m_RtcValue_mDay.m_uint8_val;
-        l_pTm_SetTemps.tm_mon   = l_TimeVal_ValRTC.m_RtcValue_mon.m_uint8_val - 1;
-        l_pTm_SetTemps.tm_isdst = -1;
-        l_pTm_SetTemps.tm_wday  = 0;
-        l_pTm_SetTemps.tm_yday  = 0;
-        l_pTm_SetTemps.tm_year  = 2025 - 1900;
-        /**/
-
-        //converti la structure dans le bon format (nombre de seconde écoulées depuis le 1er janvier 1970)
-        l_time_TimeVal = mktime(&l_pTm_SetTemps);
-
-        /**/ //regle l'orloge de la Raspberry
-        l_timeval_SetTime.tv_sec = l_time_TimeVal;
-        l_timeval_SetTime.tv_usec = 0;
-        l_int_return = settimeofday(&l_timeval_SetTime,NULL);
-        
-        if ( l_int_return < 0 ) {  
-            fprintf(l_PtrFILE_DebugFile, "l'heur n'a pas pu aitre reglé. erreur : \"%s\"\n", strerror( errno ) );
-            close(l_int_sock);
-            fclose(l_PtrFILE_DebugFile);
-            return 3;
-        }
-        /**/
-
-        close(l_int_sock);
-        fclose(l_PtrFILE_DebugFile);
-        return 0;
     }
+    /**/
+    
+    /**/ //initialise la structure de temps
+    l_pTm_SetTemps.tm_sec   = l_TimeVal_ValRTC.m_RtcValue_sec.m_uint8_val;
+    l_pTm_SetTemps.tm_min   = l_TimeVal_ValRTC.m_RtcValue_min.m_uint8_val;
+    l_pTm_SetTemps.tm_hour  = l_TimeVal_ValRTC.m_RtcValue_hour.m_uint8_val;
+    l_pTm_SetTemps.tm_mday  = l_TimeVal_ValRTC.m_RtcValue_mDay.m_uint8_val;
+    l_pTm_SetTemps.tm_mon   = l_TimeVal_ValRTC.m_RtcValue_mon.m_uint8_val - 1;
+    l_pTm_SetTemps.tm_isdst = -1;
+    l_pTm_SetTemps.tm_wday  = 0;
+    l_pTm_SetTemps.tm_yday  = 0;
+    l_pTm_SetTemps.tm_year  = 2025 - 1900;
+    /**/
+
+    //converti la structure dans le bon format (nombre de seconde écoulées depuis le 1er janvier 1970)
+    l_time_TimeVal = mktime(&l_pTm_SetTemps);
+
+    /**/ //regle l'orloge de la Raspberry
+    l_timeval_SetTime.tv_sec = l_time_TimeVal;
+    l_timeval_SetTime.tv_usec = 0;
+    l_int_return = settimeofday(&l_timeval_SetTime,NULL);
+    
+    if ( l_int_return < 0 ) {  
+        fprintf(l_PtrFILE_DebugFile, "l'heur n'a pas pu aitre reglé. erreur : \"%s\"\n", strerror( errno ) );
+        fclose(l_PtrFILE_DebugFile);
+        return 6;
+    }
+    /**/
+
+    fclose(l_PtrFILE_DebugFile);
+    return 0;
+    
 }
